921623c61b6cb37bc8b32fe9bb9c5c49e335ab70
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2024-12-30 17:07:55 -0500

    buy storage indexer mvp

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var rpc_msg = `{"jsonrpc": "2.0", "method": "%s", "id": 0, "params": {"query": "tm.event = '%s'"}}`
+var rpc_msg = `{"jsonrpc": "2.0", "method": "%s", "id": 0, "params": {"query": "tm.event = 'Tx' AND message.action = '/canine_chain.storage.MsgBuyStorage'"}}`
 
 func main() {
 	ws, _, _ := websocket.DefaultDialer.Dial("ws://127.0.0.1:26657/websocket", nil)
 	defer ws.Close()
-	go func() {
+	go func() { // display new messages
 		for {
 			_, msg, _ := ws.ReadMessage()
-			fmt.Println(string(msg))
+			println(string(msg))
 		}
 	}()
 
@@ -23,9 +23,9 @@ func main() {
 		fmt.Println("(1) Subscribe (2) Unsubscribe (3) Exit")
 		fmt.Scan(&cmd)
 		if cmd == 1 {
-			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "subscribe", "NewBlock"))) // use NewBlock to test
+			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "subscribe")))
 		} else if cmd == 2 {
-			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "unsubscribe", "NewBlock")))
+			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "unsubscribe")))
 		} else if cmd == 3 {
 			break
 		}
