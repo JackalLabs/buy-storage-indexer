f0a4ae00a88d3f5dc8534ac603cc0fdc14ab7816
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2024-12-30 16:27:59 -0500

    new block indexer

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
+var rpc_msg = `{"jsonrpc": "2.0", "method": "%s", "id": 0, "params": {"query": "tm.event = '%s'"}}`
 
 func main() {
-	fmt.Println("Auto format test")
+	ws, _, _ := websocket.DefaultDialer.Dial("ws://127.0.0.1:26657/websocket", nil)
+	defer ws.Close()
+	go func() {
+		for {
+			_, msg, _ := ws.ReadMessage()
+			fmt.Println(string(msg))
+		}
+	}()
+
+	var cmd int
+	for {
+		fmt.Println("(1) Subscribe (2) Unsubscribe (3) Exit")
+		fmt.Scan(&cmd)
+		if cmd == 1 {
+			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "subscribe", "NewBlock"))) // use NewBlock to test
+		} else if cmd == 2 {
+			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "unsubscribe", "NewBlock")))
+		} else if cmd == 3 {
+			break
+		}
+	}
 }
