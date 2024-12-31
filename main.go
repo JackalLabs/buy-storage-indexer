758b3fd6958f83f8f905f522be80528571fbacc6
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2024-12-31 10:12:27 -0500

    programming style

--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/gorilla/websocket"
 )
 
-var rpc_msg = `{"jsonrpc": "2.0", "method": "%s", "id": 0, "params": {"query": "tm.event = 'Tx' AND message.action = '/canine_chain.storage.MsgBuyStorage'"}}`
+var (
+	rpc      = `{"jsonrpc": "2.0", "method": "%s", "id": 0, "params": {"query": "%s"}}`
+	endpoint = url.URL{Scheme: "ws", Host: "127.0.0.1:26657", Path: "/websocket"}
+	query    = "tm.event = 'Tx' AND message.action = '/canine_chain.storage.MsgBuyStorage'"
+)
+
+func send(ws *websocket.Conn, method string) {
+	ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc, method, query)))
+}
 
 func main() {
-	ws, _, _ := websocket.DefaultDialer.Dial("ws://127.0.0.1:26657/websocket", nil)
+	ws, _, _ := websocket.DefaultDialer.Dial(endpoint.String(), nil)
 	defer ws.Close()
-	go func() { // display new messages
+
+	go func() { // receive messages
 		for {
-			_, msg, _ := ws.ReadMessage()
-			println(string(msg))
+			_, msg, err := ws.ReadMessage()
+			if err != nil { // handle exit error
+				break
+			}
+			fmt.Printf("%s\n", msg)
 		}
 	}()
 
 	var cmd int
+loop:
 	for {
-		fmt.Println("(1) Subscribe (2) Unsubscribe (3) Exit")
-		fmt.Scan(&cmd)
-		if cmd == 1 {
-			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "subscribe")))
-		} else if cmd == 2 {
-			ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(rpc_msg, "unsubscribe")))
-		} else if cmd == 3 {
-			break
+		fmt.Println("1: Subscribe 2: Unsubscribe 3: Exit")
+		switch fmt.Scan(&cmd); cmd {
+		case 1:
+			send(ws, "subscribe")
+		case 2:
+			send(ws, "unsubscribe")
+		case 3:
+			break loop
 		}
 	}
 }
